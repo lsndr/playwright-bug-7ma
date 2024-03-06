7438735d4ca0499041ae645f4cea61b75df9e8c3
lsndr <alexander@lsndr.me>
2024-03-07 00:26:53 +0700

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const { chromium, } = require('playwright');
+const { setTimeout } = require('timers/promises');
+
+(async () => {
+    const browser = await chromium.launch({
+        channel: 'chrome',
+        headless: true,
+    });
+
+    const page = await browser.newPage();
+
+    page.on('requestfailed', (request) => {
+        // This will hang until the browser is closed
+        request.allHeaders();
+
+        // This will be null
+        request.failure();
+    });
+
+    await page.goto('https://security-crawl-maze.app/css/font-face.html', {
+        waitUntil: 'load'
+    });
+    await page.waitForLoadState('networkidle');
+    await setTimeout(5000); // just in case
+
+    await browser.close();
+})().catch(console.error);
